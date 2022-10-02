bc03124c233b9d5c4ed5fa7d943db213311e3d64
Stefan Niewerth <stefan.niewerth@gmx.net>
2022-10-02 18:33:30 +0200

    remove myzod

--- src/types/state/system/equipment/weaponProperties.ts
+++ src/types/state/system/equipment/weaponProperties.ts
@@ -1,5 +1,3 @@
-import myzod, { Infer } from "myzod";
-
 export enum WeaponProperty {
   Breaker = "breaker",
   Compounding = "compounding",
@@ -22,88 +20,33 @@ export enum WeaponProperty {
   Vicious = "vicious",
 }
 
-const baseWeaponPropertyDescriptionSchema = myzod.object({
-  name: myzod.enum(WeaponProperty),
-});
+interface BaseProperyDescription<Property extends WeaponProperty> {
+  name: Property;
+}
 
-const breakerDescriptionSchema = baseWeaponPropertyDescriptionSchema.and(
-  myzod.object({
-    name: myzod.literal(WeaponProperty.Breaker),
-    material: myzod.string(),
-  })
-);
-export type BreakerDescription = Infer<typeof breakerDescriptionSchema>;
+export interface BreakerDescription extends BaseProperyDescription<WeaponProperty.Range> {}
 
-const defensiveDescriptionSchema = baseWeaponPropertyDescriptionSchema.and(
-  myzod.object({
-    name: myzod.literal(WeaponProperty.Defensive),
-    max: myzod.string(), // TODO Shield PropertyEnum
-  })
-);
-export type DefensiveDescription = Infer<typeof defensiveDescriptionSchema>;
+export interface DefensiveDescription extends BaseProperyDescription<WeaponProperty.Range> {}
 
-const rangeDescriptionSchema = baseWeaponPropertyDescriptionSchema.and(
-  myzod.object({
-    name: myzod.literal(WeaponProperty.Range),
-    normal: myzod.number({ min: 0 }),
-    long: myzod.number({ min: 0 }),
-  })
-);
-export type RangeDescription = Infer<typeof rangeDescriptionSchema>;
+export interface RangeDescription extends BaseProperyDescription<WeaponProperty.Range> {}
 
-const reachDescriptionSchema = baseWeaponPropertyDescriptionSchema.and(
-  myzod.object({
-    name: myzod.literal(WeaponProperty.Reach),
-    reach: myzod.number({ min: 0 }),
-  })
-);
-export type ReachDescription = Infer<typeof reachDescriptionSchema>;
+export interface ReachDescription extends BaseProperyDescription<WeaponProperty.Range> {}
 
-const thrownDescriptionSchema = baseWeaponPropertyDescriptionSchema.and(
-  myzod.object({
-    name: myzod.literal(WeaponProperty.Thrown),
-    normal: myzod.number({ min: 0 }),
-    long: myzod.number({ min: 0 }),
-  })
-);
-export type ThrownDescription = Infer<typeof thrownDescriptionSchema>;
+export interface ThrownDescription extends BaseProperyDescription<WeaponProperty.Range> {}
 
-const versatileDescriptionSchema = baseWeaponPropertyDescriptionSchema.and(
-  myzod.object({
-    name: myzod.literal(WeaponProperty.Versatile),
-    damage: myzod.object({}), // TODO,
-  })
-);
-export type VersatileDescription = Infer<typeof versatileDescriptionSchema>;
+export interface VersatileDescription extends BaseProperyDescription<WeaponProperty.Range> {}
 
-const BasePropertiesMapping = Object.values(WeaponProperty).reduce(
-  (prev, curr) => ({ ...prev, [curr]: otherWeaponPropteryDescriptionSchemas }),
-  {} as Record<WeaponProperty, typeof otherWeaponPropteryDescriptionSchemas>
-);
+interface ExtendedWeaponProperties {
+  [WeaponProperty.Breaker]: BreakerDescription;
+  [WeaponProperty.Defensive]: DefensiveDescription;
+  [WeaponProperty.Range]: RangeDescription;
+  [WeaponProperty.Reach]: ReachDescription;
+  [WeaponProperty.Thrown]: ThrownDescription;
+  [WeaponProperty.Versatile]: VersatileDescription;
+}
 
-const otherWeaponProperties = Object.values(WeaponProperty).filter(
-  (property): property is Exclude<WeaponProperty, keyof typeof ExtendedWeaponProperties> =>
-    property in ExtendedWeaponProperties
-);
-export const otherWeaponPropteryDescriptionSchemas = myzod.object({
-  name: myzod.literals(...otherWeaponProperties),
-});
-
-const ExtendedWeaponProperties = {
-  [WeaponProperty.Breaker]: breakerDescriptionSchema,
-  [WeaponProperty.Defensive]: defensiveDescriptionSchema,
-  [WeaponProperty.Range]: rangeDescriptionSchema,
-  [WeaponProperty.Reach]: reachDescriptionSchema,
-  [WeaponProperty.Thrown]: thrownDescriptionSchema,
-  [WeaponProperty.Versatile]: versatileDescriptionSchema,
-};
-
-const AllWeaponPropertziesMapping = {
-  ...BasePropertiesMapping,
-  ...ExtendedWeaponProperties,
-};
-
-export const weaponPropertySchema = baseWeaponPropertyDescriptionSchema.map((weaponProperty) => {
-  return AllWeaponPropertziesMapping[weaponProperty.name].parse(weaponProperty);
-});
-export type WeaponPropertyDescription = Infer<typeof weaponPropertySchema>;
+export type WeaponPropertyDescription = {
+  [Property in WeaponProperty]: Property extends keyof ExtendedWeaponProperties
+    ? ExtendedWeaponProperties[Property]
+    : BaseProperyDescription<Property>;
+}[WeaponProperty];
