b9fcbb590a8f9b54a11db7bd2e9a65516c548547
Stefan Niewerth <stefan.niewerth@gmx.net>
2023-02-17 10:28:02 +0100

    cleanup stuff

--- src/hooks/useMount.ts
+++ src/hooks/useMount.ts
@@ -1,6 +1,6 @@
 import { EffectCallback, useEffect } from 'react';
 
-export function useMount(callback: EffectCallback) {
+export default function useMount(callback: EffectCallback) {
 	// eslint-disable-next-line react-hooks/exhaustive-deps
 	useEffect(callback, []);
 }
