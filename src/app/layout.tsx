c9dc736ed4f6c9881e8eea0dbc7bcad13e50c062
Stefan Niewerth <stefan.niewerth@gmx.net>
2023-07-08 11:19:12 +0200

    stylefix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,8 @@ import './globals.css';
 
 export const metadata: Metadata = {
 	title: 'A5E Character Creator',
-	icons: ['/favicon.ico']
-  }
+	icons: ['/favicon.ico'],
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
