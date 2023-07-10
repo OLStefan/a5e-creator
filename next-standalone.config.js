163184689c7ccac32aab55afdb89329fd660bf9c
Stefan Niewerth <stefan.niewerth@gmx.net>
2023-07-10 09:49:03 +0200

    add baseConfig

--- next-standalone.config.js
+++ next-standalone.config.js
@@ -3,12 +3,7 @@ import baseConfig from './next.config.bak.js';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	...baseConfig,
-	output: 'export',
-	basePath: process.env.BASE_PATH,
-	env: {
-		...baseConfig.env,
-		NEXT_PUBLIC_BASE_PATH: process.env.BASE_PATH,
-	},
+	output: 'standalone',
 };
 
 export default nextConfig;
