edfc6d63f0b001585cba5a19d99debd0b2455da4
Stefan Niewerth <stefan.niewerth@gmx.net>
2023-08-25 21:40:34 +0200

    add id for style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 	return (
 		<html lang="en">
 			<head />
-			<body className={inter.className}>
+			<body className={inter.className} id="a5e">
 				<StyledComponentsRegistry>
 					<AntStyleConfig>
 						<App>
