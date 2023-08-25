cc182d5aedd47aaacecf09985541e4426373d90d
Stefan Niewerth <stefan.niewerth@gmx.net>
2023-08-25 22:58:13 +0200

    fights with CSS

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,16 +1,19 @@
 module.exports = {
-	plugins: {
-		'postcss-plugin-namespace': [':global(#a5e)', { ignore: [':root'] }],
-		'postcss-flexbugs-fixes': {},
-		'postcss-preset-env': {
-			autoprefixer: {
-				flexbox: 'no-2009',
+	plugins: [
+		['postcss-plugin-namespace', true],
+		'postcss-flexbugs-fixes',
+		[
+			'postcss-preset-env',
+			{
+				autoprefixer: {
+					flexbox: 'no-2009',
+				},
+				stage: 3,
+				features: {
+					'custom-properties': false,
+				},
 			},
-			stage: 3,
-			features: {
-				'custom-properties': false,
-			},
-		},
-		'postcss-nesting': {},
-	},
+		],
+		'postcss-nesting',
+	],
 };
