1ec46b40395b2f3df4c81db566e08dd1d79930b2
Stefan Niewerth <stefan.niewerth@gmx.net>
2023-11-19 12:23:05 +0100

    more vehicle capacity stuff

--- src/data/models/equipment/vehicles.ts
+++ src/data/models/equipment/vehicles.ts
@@ -1,6 +1,7 @@
-import { types } from 'mobx-state-tree';
+import { mapValues, pick } from 'lodash';
+import { Instance, SnapshotIn, types } from 'mobx-state-tree';
 import { Size } from '../general';
-import { equipmentPieceModel, EquipmentType } from './base';
+import { EquipmentType, equipmentPieceModel } from './base';
 import { materialModel } from './material';
 import { vehiclePropertyModel } from './vehicleProperties';
 
@@ -32,20 +33,61 @@ export const vehicleModel = types.compose(
 		})
 		.extend((self) => {
 			const views = {
-				get carryingCapacity() {
-					const base = { crew: self.crew };
+				get personCapacity() {
+					if (isPersonal(self)) {
+						return {
+							crew: 1,
+							passengers: 0,
+						};
+					}
 
+					const base = pick(self, 'crew');
 					switch (self.size) {
 						case Size.Large:
-							return { ...base, passengers: 3, supply: 40, bulky: 10, additional: 2000 };
+							return { ...base, passengers: 3 };
 						case Size.Huge:
-							return { ...base, passengers: 6, supply: 80, bulky: 20, additional: 4000 };
+							return { ...base, passengers: 6 };
 						case Size.Gargantuan:
-							return { ...base, passengers: self.crew, supply: 800, bulky: 200, additional: 40000 };
+							return { ...base, passengers: self.crew };
 					}
+				},
+				get cargoCapacity() {
+					const sizeBased = getSizeBasedCargoCapacity(self);
+
+					if (isPersonal(self)) {
+						return {
+							...mapValues(sizeBased, (value) => value / 4),
+						};
+					}
+
+					return sizeBased;
+				},
+				get carryingCapacity() {
+					return {
+						...views.personCapacity,
+						...views.cargoCapacity,
+					};
 				},
 			};
 
 			return { views };
 		}),
 );
+
+export interface Vehicle extends Instance<typeof vehicleModel> {}
+export interface VehicleSnapshot extends SnapshotIn<typeof vehicleModel> {}
+
+function isPersonal(vehicle: Pick<Vehicle, 'properties'>) {
+	return vehicle.properties.some((prop) => prop.name === 'Personal');
+}
+
+function getSizeBasedCargoCapacity(vehicle: Pick<Vehicle, 'size'>) {
+	switch (vehicle.size) {
+		case Size.Large:
+			return { supply: 40, bulky: 10, additionalCargo: 2000 };
+		case Size.Huge:
+			return { supply: 80, bulky: 20, additionalCargo: 4000 };
+		case Size.Gargantuan:
+			return { supply: 800, bulky: 200, additionalCargo: 40000 };
+	}
+}
