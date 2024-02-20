5941fc45193dad53fd1081bb7eeabeb29c953c91
Stefan Niewerth <stefan.niewerth@gmx.net>
2024-02-20 18:59:16 +0100

    stable callback

--- src/app/(components)/MarkdownEscape.tsx
+++ src/app/(components)/MarkdownEscape.tsx
@@ -1,23 +1,24 @@
 'use client';
 
 import { Divider, Input } from 'antd';
+import { TextAreaProps } from 'antd/es/input';
 import { observer } from 'mobx-react-lite';
+import { useUpdatingCallbacks } from 'use-updating-callbacks';
 import { useTextModel } from './TextProvider';
 import styles from './markdown.module.css';
 
 export default observer(function MarkdownEscape() {
 	const model = useTextModel();
 
+	const callbacks = useUpdatingCallbacks({
+		onChange: ((event) => {
+			model.update(event.target.value);
+		}) satisfies TextAreaProps['onChange'],
+	});
+
 	return (
 		<div className={styles.root}>
-			<Input.TextArea
-				value={model.text}
-				className={styles['text-area']}
-				onChange={(event) => {
-					model.update(event.target.value);
-				}}
-				autoSize
-			/>
+			<Input.TextArea value={model.text} className={styles['text-area']} onChange={callbacks.onChange} autoSize />
 			<Divider />
 			<div>{model.escaped}</div>
 			<Divider />
